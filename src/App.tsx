460480e640a3065c9a99494e693b96db3a5498ce
Ansh Singh <74236609+Anshsing@users.noreply.github.com>
2026-06-23 18:51:54 +0530

    Set basename for BrowserRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/localist-landing-page">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
